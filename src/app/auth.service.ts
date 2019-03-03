4b35d08f03a8eb4642f8d04e5413f5dfaae4f255
codename434 <39920983+codename434@users.noreply.github.com>
2019-03-04 02:47:14 +0530

    Log out button and admin link fix

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,11 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthService {
   $user : Observable<firebase.User>;
   
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
-    this.$user = afAuth.authState;
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute) { 
+      this.$user = afAuth.authState;
   }
 
   login(){
@@ -22,5 +28,15 @@ export class AuthService {
 
   logout(){
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser>{
+    return this.$user
+    .pipe(
+      switchMap(user => {
+        if(user) return this.userService.get(user.uid);
+        
+        return of(null);
+      }));
   }
 }
