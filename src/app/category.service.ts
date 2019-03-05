cccef45ebe3c029fb3c63a40b58522e5eab136ca
codename434 <39920983+codename434@users.noreply.github.com>
2019-03-06 01:42:35 +0530

    product category save fix

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ export class CategoryService {
   constructor(private db: AngularFireDatabase) { }
 
   getCategories(){
-    return this.db.list('/categories', queryFn => queryFn.orderByChild('name')).valueChanges();
+    return this.db.list('/categories', queryFn => queryFn.orderByChild('name')).snapshotChanges()
+    .pipe(map(changes => {
+      return changes.map(c=> ({key: c.payload.key, value: c.payload.val()}))
+    }));
   }
 }
