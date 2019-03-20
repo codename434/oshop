3f8bc46e8ba17616485b39a088af59816fdc2ddc
codename434 <39920983+codename434@users.noreply.github.com>
2019-03-20 11:21:03 +0530

    category list and active class added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ import { ProductService } from './product.service';
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
-      { path:'', component: HomeComponent },
+      { path:'', component: ProductsComponent },
       { path:'products', component: ProductsComponent },
       { path:'login', component: LoginComponent },
       { path:'shopping-cart', component: ShoppingCartComponent },
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
+  categories$;
+  category : string;
 
-  constructor() { }
+  constructor(
+    route: ActivatedRoute,
+    productService : ProductService, 
+    categoryService : CategoryService) { 
 
-  ngOnInit() {
+    this.products$ = productService.getAll();
+    this.categories$ = categoryService.getCategories();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+    });
   }
 
 }
