efe4c01fc5729b78f8b84a3b20878991f76c893e
codename434 <39920983+codename434@users.noreply.github.com>
2019-04-04 11:12:27 +0530

    number of shopping cart items navbar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser : AppUser;
-  constructor(private auth: AuthService) { 
-    auth.appUser$.subscribe(appUser => this.appUser = appUser );
-   }
+  shoppingCartItemCount: number;
+
+  constructor(private auth: AuthService, private cartService: ShoppingCartService) { 
+    
+  }
+
+  async ngOnInit(){
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser );
+    
+    let cart$ = await this.cartService.getCart();
+    cart$.subscribe((cart : any)=> {
+      this.shoppingCartItemCount = 0;
+      for(let productId in cart.payload.val().items){
+        this.shoppingCartItemCount += cart.payload.val().items[productId].quantity;
+      }
+    })
+  }
 
   logout(){
     this.auth.logout();
