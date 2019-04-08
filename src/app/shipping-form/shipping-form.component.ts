3924339c0e944af354d14519700aa89a66e6017f
codename434 <39920983+codename434@users.noreply.github.com>
2019-04-08 11:44:42 +0530

    refractored shipping form component

---
+++ src/app/shipping-form/shipping-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { OrderService } from '../order.service';
+import { Order } from '../models/order';
+import { ShoppingCart } from '../models/shopping-cart';
+
+@Component({
+  selector: 'shipping-form',
+  templateUrl: './shipping-form.component.html',
+  styleUrls: ['./shipping-form.component.css']
+})
+export class ShippingFormComponent implements OnInit, OnDestroy {
+  shipping = {};
+  subscription: Subscription;
+  userId;
+  @Input('cart') cart: ShoppingCart;
+  
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private orderService: OrderService,
+    ) { }
+
+  ngOnInit() {
+    this.subscription = this.authService.$user.subscribe(user => this.userId = user.uid);
+  }
+
+  
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  async placeOrder(){
+    let order = new Order(this.userId, this.shipping, this.cart);
+    let result = await this.orderService.placeOrder(order);
+    this.router.navigate(['order-success', result.key]);
+  }
+
+}
