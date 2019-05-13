3e37d726f53dc443b8eff7901b5427b573bdc1f8
codename434 <39920983+codename434@users.noreply.github.com>
2019-05-13 15:52:27 +0530

    Shopping module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,16 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { CustomFormsModule } from 'ng2-validation';
-import { AuthGuard } from 'shared/services/auth-guard.service';
 import { SharedModule } from 'shared/shared.module';
 
 import { environment } from './../environments/environment';
 import { AdminModule } from './admin/admin.module';
-import { AdminOrdersComponent } from './admin/component/admin-orders/admin-orders.component';
-import { AdminProductsComponent } from './admin/component/admin-products/admin-products.component';
-import { ProductFormComponent } from './admin/component/product-form/product-form.component';
-import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductsComponent } from './products/products.component';
-import { ShippingFormComponent } from './shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './shopping/components/products/products.component';
+import { ShoppingModule } from './shopping/shopping.module';
 
 
 @NgModule({
@@ -35,39 +24,22 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
     
-    LoginComponent,
     
-    ProductFilterComponent,
-    ShoppingCartSummaryComponent,
-    ShippingFormComponent
+    LoginComponent
+    
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
+    ShoppingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
       { path:'', component: ProductsComponent },
-      { path:'products', component: ProductsComponent },
       { path:'login', component: LoginComponent },
-      { path:'shopping-cart', component: ShoppingCartComponent },
-
-      { path:'my/orders', component: MyOrdersComponent, canActivate: [ AuthGuard ]},
-      { path:'check-out', component: CheckOutComponent, canActivate: [ AuthGuard ] },
-      { path:'order-success/:id', component: OrderSuccessComponent, canActivate: [ AuthGuard ]  },
-      
-      { path:'admin/products/new', component: ProductFormComponent, canActivate: [ AuthGuard, AdminAuthGuard ]  },
-      { path:'admin/products/:id', component: ProductFormComponent, canActivate: [ AuthGuard, AdminAuthGuard ]  },
-      { path:'admin/products', component: AdminProductsComponent, canActivate: [ AuthGuard, AdminAuthGuard ]  },
-      { path:'admin/orders', component: AdminOrdersComponent, canActivate: [ AuthGuard, AdminAuthGuard]  },
     ]),
     NgbModule.forRoot(),
     FormsModule,
